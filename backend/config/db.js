4b1c5cbcb0c2f504aa74a2e41010a0378406df4b
viraj0407 <virajchavan177@gmail.com>
2025-05-23 14:24:34 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,23 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-dotenv.config();
 
 export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
-    process.exit(1);
-  }
-};
+    await mongoose.connect('mongodb+srv://chavanviraj475:viraj0407@cluster0.7cuo6.mongodb.net/wholesale-grocery-store').then(()=>console.log("DB connected"))
+}
+
+
+
+// import dotenv from "dotenv";
+
+// Load environment variables from .env file
+// dotenv.config();
+
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error("❌ MongoDB connection error:", error.message);
+//     process.exit(1);
+//   }
+// };
