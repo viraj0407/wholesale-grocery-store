3ec98be63469f6f3faa885da5731d7173c7a162c
viraj0407 <virajchavan177@gmail.com>
2025-05-23 13:55:14 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
